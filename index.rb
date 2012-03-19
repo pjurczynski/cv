d8889afbdbd2a6d58da3dd4f44ca6305d2051a06
Peter Jurczyński <pjurczynski@gmail.com>
2012-03-19 19:33:44 +0100

    complete polish support

--- index.rb
+++ index.rb
@@ -2,29 +2,33 @@ require 'sinatra'
 require 'haml'
 require 'sass'
 require 'sinatra/content_for'
+require 'sinatra/namespace'
 
 before do
-  @suffix = ''
+  @prefix = ''
+  @url = request.path_info
 end
 
-get '/' do
+get '/?' do
   @body_class = :cv
   haml :index
 end
 
-get '/pl' do
-  @body_class = :cv
-  @suffix = '/pl'
-  haml :index_pl
-end
-
-get '/portfolio' do
+get '/portfolio/?' do
   @body_class = :slides
   haml :portfolio
 end
 
-get '/portfolio/pl' do
-  @body_class = :slides
-  @suffix = '/pl'
-  haml :portfolio_pl
+namespace '/pl/?' do
+  get do
+    @body_class = :cv
+    @prefix = '/pl'
+    haml :index_pl
+  end
+
+  get '/portfolio/?' do
+    @body_class = :slides
+    @prefix = '/pl'
+    haml :portfolio_pl
+  end
 end
