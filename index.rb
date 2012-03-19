f434e7979760f8a2f0fb4b2ec229f1e1c98e5d16
Peter Jurczyński <pjurczynski@gmail.com>
2012-03-19 08:40:33 +0100

    cleaning layout templates and introducing polish version

--- index.rb
+++ index.rb
@@ -1,15 +1,25 @@
 require 'sinatra'
 require 'haml'
 require 'sass'
+require 'sinatra/content_for'
 
 get '/' do
+  @body_class = :cv
   haml :index
 end
 
-get '/portfolio' do
-  haml :portfolio, :layout => :slides
+get '/pl' do
+  @body_class = :cv
+  haml :index_pl
 end
 
-get '/test' do
-  haml :test, :layout => :clean
+# TODO: Why this isn't working? @body_class
+get '/portfolio' do
+  @body_class = :slides
+  haml :portfolio
+end
+
+get '/portfolio/pl' do
+  @body_class = :slides
+  haml :portfolio, :layout => :slides
 end
